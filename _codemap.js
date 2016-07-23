52effdbc82d4a9ff52e7aa229b1c25c4b3e73f6e
Carlos Rodriguez <carlos@s8f.org>
2016-07-22 20:11:58 -0700

    config

--- _codemap.js
+++ _codemap.js
@@ -1,7 +1,13 @@
+var path = require('path')
+
 module.exports = {
   _ns: 'zenbot',
   _maps: [
     require('./core/_codemap'),
     require('./utils/_codemap')
-  ]
+  ],
+  'trader.config': function container (get, set, clear) {
+    var p = path.join(__dirname, 'config-trader-sample.js')
+    return get('utils.make_config')(p)
+  }
 }
